931e9c666e77381460b44078d13bd8f94f4d04e6
Luke Hsiao <lwhsiao@stanford.edu>
2018-10-25 12:55:46 -0700

    Clean up rojadirecta code

--- src/streamscrape/other/rojadirecta.py
+++ src/streamscrape/other/rojadirecta.py
@@ -36,36 +36,12 @@ def scrape():
         elif href.startswith("http://it.rojadirecta.eu/goto/"):
             href = href.replace("it.rojadirecta.eu/goto/", "")
 
-        event_data = (get_utc_time_str(), href, get_ip_address(href))
+        event_data = {
+            "timestamp": get_utc_time_str(),
+            "url": href,
+            "ip": get_ip_address(href),
+        }
         logger.debug("RojaDirecta URL data: {}".format(event_data))
         all_urls.append(event_data)
 
     return all_urls
-
-    #  def scrape():
-    #      """Scrape LiveTV.
-    #
-    #      :return: A list of {"timestamp": _, "url": _, "ip": _}
-    #      """
-    #      page = requests.get(HOME)
-    #      soup = BeautifulSoup(page.text, "html.parser")
-    #
-    #      # Gather current live URLs from the English ("enx") version of the page.
-    #      live_urls = set(a.attrs["href"] for a in soup.find_all("a", class_="live"))
-    #
-    #      import pdb
-    #
-    #      pdb.set_trace()
-
-    #
-    #  all_urls = []
-    #
-    #  # Process all urls in parallel.
-    #  with mp.Pool(2 * mp.cpu_count()) as p:
-    #      results = p.map(_scrape_event, live_urls)
-    #      for event_urls in results:
-    #          all_urls.extend(url for url in event_urls)
-    #
-    #  logger.debug("{}".format(pformat(all_urls)))
-
-    return
